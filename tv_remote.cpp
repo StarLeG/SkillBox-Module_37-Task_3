89e563cd48200bc9c5afdc2382798f6f51d771f9
Oleg Starinsky <oleg.starynski@gmail.com>
2023-01-05 13:17:53 +0300

    Re-init

--- tv_remote.cpp
+++ tv_remote.cpp
@@ -13,8 +13,92 @@ TV_Remote::~TV_Remote()
     delete ui;
 }
 
-void TV_Remote::on_Bnt_0_clicked()
+void TV_Remote::on_ButtonDigit_clicked()
 {
+    QString str = ((QPushButton*)sender())->text();;
+    if (str.contains(QRegExp("[0-9]"))) {
+           str_buffer += str;
+
+           if(str_buffer.length() > 3 )
+           {
+               str_buffer.clear();
+           }
+       }
+   qDebug() << str_buffer.toInt();
+   channel = str_buffer.toInt();
+   ui->Channel_display->setText(str_buffer);
+}
+
+
+void TV_Remote::on_Btn_vol_Up_clicked()
+{
+    if(volume < 100)
+    {
+        volume++;
+    }
+    else
+    {
+        volume = 100;
+
+    }
+
+    qDebug()<< "Volume = " << volume;
+    str_volume = QString::number(volume);
+    ui->Volume_display->setText(str_volume);
+
+}
+
+
+void TV_Remote::on_Btn_vol_Down_clicked()
+{
+    if(volume > 0)
+    {
+        volume--;
+    }
+    else
+    {
+        volume = 0;
+    }
+    qDebug()<< "Volume = " << volume;
+    str_volume = QString::number(volume);
+    ui->Volume_display->setText(str_volume);
+}
+
+
+void TV_Remote::on_Btn_ch_Up_clicked()
+{
+    if(channel < 999)
+    {
+        channel++;
+    }
+    else
+    {
+        channel = 0;
+
+    }
+
+    qDebug() << "Channel = " << channel;
+    str_channel = QString::number(channel);
+    ui->Channel_display->setText(str_channel);
+}
+
+
+void TV_Remote::on_Btn_ch_Down_clicked()
+{
+    if(channel > 0)
+    {
+        channel--;
+    }
+    else
+    {
+        channel = 999;
+
+    }
+
+    qDebug() << "Channel = " << channel;
+    str_channel = QString::number(channel);
+    ui->Channel_display->setText(str_channel);
+
 
 }
 
--- tv_remote.h
+++ tv_remote.h
@@ -2,6 +2,7 @@
 #define TV_REMOTE_H
 
 #include <QMainWindow>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class TV_Remote; }
@@ -17,9 +18,22 @@ public:
 
 private slots:
 
-    void on_Bnt_0_clicked();
+    void on_ButtonDigit_clicked();
+
+    void on_Btn_vol_Up_clicked();
+
+    void on_Btn_vol_Down_clicked();
+
+    void on_Btn_ch_Up_clicked();
+
+    void on_Btn_ch_Down_clicked();
 
 private:
     Ui::TV_Remote *ui;
+    int volume = 0;
+    int channel = 0;
+    QString str_volume;
+    QString str_channel;
+    QString str_buffer;
 };
 #endif // TV_REMOTE_H
